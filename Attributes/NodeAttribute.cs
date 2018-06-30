41b672e3023a046b435c03e047cc2eb7f5b83c6e
Patrik Lindberg <patriksmsn@hotmail.com>
2018-06-30 18:26:34 +0200

    Removed todo-comment.

--- Attributes/NodeAttribute.cs
+++ Attributes/NodeAttribute.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 
-// TODO: Overrides for width and height and similar basic layout properties, so that
-//		 some level of customization is possible without creating a custom editor script.
-
 [AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
 public class NodeAttribute : Attribute
 {
