b9a3300eda3ee0e249ad0293c4846080f899d6ca
Patrik Lindberg <patrik.lindberg@starstable.com>
2018-08-27 16:05:22 +0200

    Added back double-click to open graphs.

---
+++ Editor/AssetHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using System.Linq;
+
+namespace DataDesigner
+{
+	public class MyAssetHandler
+	{
+		[OnOpenAssetAttribute(1)]
+		public static bool step1(int instanceID, int line)
+		{
+			var asset = EditorUtility.InstanceIDToObject(instanceID);
+			if (asset.GetType().GetCustomAttributes(typeof(GraphAttribute), true).Any())
+			{
+				NodeEditor.OpenWindow(asset);
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
